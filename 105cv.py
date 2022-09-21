cb74a881c1ccc46c165a5230ea402c4fce66f1de
Nehakumari-8 <76731799+Nehakumari-8@users.noreply.github.com>
2022-09-21 20:59:42 +0530

    Add files via upload

---
+++ 105cv.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+img= cv2.imread("butterfly.jpg")
+
+cv2.imshow("display image",img)
+cv2.waitKey(4000)
+
+
+
+
+
+
+
+        
+
+#print(h,w,c)
+#video = cv2.video
+
+#cv2.imshow("output",img)
+
+    #cv2.imshow("output",img1)
+    
+#black = np.zeros([600,600])
+#black[200:400,200:400]=200
+#cv2.imshow("output",black)
+#print(black)
+#cv2.waitKey(4000)
+
+
+
+#cv2.waitKey(4000)    
+
